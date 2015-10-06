b677f896fb7b090a0eb5606a6d0f22e4469b9996
Michael Hahn <mike@telhill.com>
2015-10-06 08:58:04 -0700

    Initial commit.

---
+++ src/main/java/com/mapr/ojai/samples/EX01JSON_READ.java
@@ -0,0 +1,12 @@
+package com.mapr.ojai.samples;
+
+/**
+ * Created by michaelHahn on 10/5/15.
+ */
+public class EX01JSON_READ {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
